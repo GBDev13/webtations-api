e25115a4d7edb9f3046fd238de2401c39c6d0ff1
Gabriel Borges <gbdesigns13@gmail.com>
2021-01-22 20:06:59 -0300

    Primeiro deploy

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => console.log('Connection succesful'))
+  .catch((err) => console.log(err))
